93e201cb47fb82904dc83144a64233fdd876a9f1
Rohith-WebCode <rohithprakash19@gmail.com>
2026-07-04 10:02:22 +0530

    add interactive 3D product viewer with model switching

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLinks } from '../Constants'
+import { navLinks } from '../Constants'
 
 const NavBar = () => {
   return ( 
@@ -7,7 +7,7 @@ const NavBar = () => {
         <nav>
           <img src='/logo.svg' alt='apple logo'/>
           <ul>
-            {NavLinks.map(({label})=>(
+            {navLinks.map(({label})=>(
               <li key={label}>
                <a href={label}>{label}</a>
               </li>
